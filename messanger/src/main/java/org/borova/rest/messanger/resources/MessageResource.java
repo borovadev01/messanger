b6e5882a89f9f5a2d1095752d2c344785e7b32fe
borova <borovadev01@gmail.com>
2016-05-24 16:46:23 +0200

    add @PathParam

--- messanger/src/main/java/org/borova/rest/messanger/resources/MessageResource.java
+++ messanger/src/main/java/org/borova/rest/messanger/resources/MessageResource.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -18,7 +19,13 @@ public class MessageResource {
 	@GET
 	@Produces(MediaType.APPLICATION_XML)
 	public List<Message> getMessages() {
-		System.out.println(messageService.getAllMessages());
 		return messageService.getAllMessages();
+	}
+
+	@GET
+	@Path("/{messageId}")
+	@Produces(MediaType.APPLICATION_XML)
+	public Message getMessage(@PathParam("messageId") long id) {
+		return messageService.getMessage(id);
 	}
 }
